19778f061a983e812a536c9a438d20753d063709
maxsg <msteing@gmail.com>
2025-01-24 14:16:52 -0500

    Include tailwind conf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        bluePalette: {
+          50: "#eff6fe",
+          100: "#e0ecfc",
+          200: "#c1dafa",
+          300: "#a1c7f7",
+          400: "#82b5f5",
+          500: "#63a2f2",
+          600: "#4f82c2",
+          700: "#3b6191",
+          800: "#284161",
+          900: "#142030",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
